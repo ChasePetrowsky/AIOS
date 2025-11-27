63cc443d036c2d633299b06dd0441c9901e44cfb
Medo-com <ahmedsaid11213@gmail.com>
2025-11-27 13:15:05 -0700

    Initial commit

---
+++ Controllers/InventoryController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class InventoryController : Controller
+{
+    public IActionResult Index() => View();     // Vehicle list UI
+    public IActionResult Add() => View();       // Add vehicle form UI
+    public IActionResult Details() => View();   // Vehicle details UI
+}
